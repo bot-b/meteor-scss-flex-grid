be597672cafce1170961d40d52677e15fa7af265
Matthew Simo <matthew.a.simo@gmail.com>
2015-02-28 13:28:10 -0600

    scss flex grid packaged for meteor.

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'matthewsimo:scss-flex-grid',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Adds scss-flex-grid',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.2');
+
+	api.use('fourseven:scss', 'client');
+	api.imply('fourseven:scss');
+
+	api.addFiles('scss.json', 'server');
+	api.addFiles('scss-flex-grid/_flex-grid.scss', 'server', { isAsset: true });
+});
+
+Package.onTest(function(api) {
+});
